b3ae93eed6ade12129ca6eea2c564094d6b6af07
yuanguan <yuan.guan@edu.sasky.fi>
2023-08-29 23:19:16 +0300

    lisää Content-komponentin

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,6 +2,8 @@ import Menu from '../Menu'
 import Header from '../Header'
 import styles from './App.module.scss'
 import Item from '../Item'
+import Content from '../Content'
+
 
 function App() {
 
@@ -9,14 +11,17 @@ function App() {
     <>
       <div className={styles.app}>
         <Header />
-        <Item />
-        <Item />
-        <Item />
-        <Item />
+        <Content>
+          <Item />
+          <Item />
+          <Item />
+          <Item />
+        </Content>
         <Menu />
       </div>
     </>
   )
+
 }
 
 export default App
