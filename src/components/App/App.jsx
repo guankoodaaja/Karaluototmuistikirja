288142870fb2acd7ff327d7cdc4bcae9d110f592
yuanguan <yuan.guan@edu.sasky.fi>
2023-09-11 16:45:37 +0300

    lisää Settings-komponentin

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,25 +1,26 @@
 import styles from './App.module.scss'
+import { ButtonContainer } from '../../shared/buttons'
 import Content from '../Content'
 import Header from '../Header'
-import Item from '../Item'
+import Items from '../Items'
 import Menu from '../Menu'
+import Settings from '../Settings'
+import Stats from '../Stats'
 import { FloatingButton } from '../../shared/buttons'
 
 function App() {
 
   return (
     <>
+      <ButtonContainer>
       <div className={styles.app}>
         <Header />
         <Content>
-          <Item />
-          <Item />
-          <Item />
-          <Item />
-          <FloatingButton secondary>+</FloatingButton>
+          <Settings />
         </Content>
         <Menu />
       </div>
+      </ButtonContainer>
     </>
   )
 }
